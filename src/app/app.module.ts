0ed05a1399d9dbb68e52adfda933121f864e2063
Niklas Wolf <niklas.wolf@plentymarkets.com>
2019-09-23 21:53:34 +0200

    reactiveFormElements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     AppComponent,
     NavigationComponent,
     DashboardComponent,
-    EnterDataComponent
+    EnterDataComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/enter-data/enter-data.component.ts
+++ src/app/enter-data/enter-data.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { EnterDataInterface } from '../enter-data.interface';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-enter-data',
@@ -9,8 +10,20 @@ import { EnterDataInterface } from '../enter-data.interface';
 })
 export class EnterDataComponent implements OnInit {
 
-  public EnterData: EnterDataInterface;
   constructor() { }
+
+  public EnterData: EnterDataInterface;
+
+  dataForm = new FormGroup({
+    amount: new FormControl(''),
+    persOrComp: new FormControl(''),
+    titel: new FormControl(''),
+});
+
+  onSubmit() {
+    alert(this.dataForm.value);
+  }
+
 
   // Routing
   private navigate: DashboardComponent;

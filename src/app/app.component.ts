52b885febec96c0c748bc131471976ea62f17d66
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-09-27 12:57:03 +0200

    move list of items to the dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,18 +14,4 @@ import { Item } from './models/item.interface';
 export class AppComponent
 {
   title = 'finman';
-
-  private itemsCollection: AngularFirestoreCollection<Item>;
-  items: Observable<Item[]>;
-
-  constructor(private afs: AngularFirestore)
-  {
-    this.itemsCollection = afs.collection<Item>('items');
-    this.items = this.itemsCollection.valueChanges();
-  }
-
-  addItem(item: Item)
-  {
-    this.itemsCollection.add(item);
-  }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection
+} from '@angular/fire/firestore';
+import { Item } from '../models/item.interface';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,4 +12,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
+
+  constructor(private afs: AngularFirestore)
+  {
+    this.itemsCollection = afs.collection<Item>('items');
+    this.items = this.itemsCollection.valueChanges();
+  }
+
+  addItem(item: Item)
+  {
+    this.itemsCollection.add(item);
+  }
 }

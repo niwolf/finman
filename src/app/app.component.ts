b4cbf2daff089b3b7a0615e95befb687706aeddf
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-09-28 19:50:08 +0200

    move stuff from AppComponent to dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,4 @@
 import { Component } from '@angular/core';
-import {
-  AngularFirestore,
-  AngularFirestoreCollection
-} from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-
-
-export interface Item
-{
-  title: string;
-  value: number;
-  createdAt: Date;
-  category: number;
-  user: number;
-}
 
 @Component({
   selector:    'fin-app',
@@ -23,18 +8,4 @@ export interface Item
 export class AppComponent
 {
   title = 'finman';
-
-  private itemsCollection: AngularFirestoreCollection<Item>;
-  items: Observable<Item[]>;
-
-  constructor(private afs: AngularFirestore)
-  {
-    this.itemsCollection = afs.collection<Item>('items');
-    this.items = this.itemsCollection.valueChanges();
-  }
-
-  addItem(item: Item)
-  {
-    this.itemsCollection.add(item);
-  }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,18 @@
 import { Component } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Item
+{
+  title: string;
+  value: number;
+  createdAt: Date;
+  category: number;
+  user: number;
+}
 
 @Component({
   selector: 'fin-dashboard',
@@ -6,4 +20,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
+
+  constructor(private afs: AngularFirestore)
+  {
+    this.itemsCollection = afs.collection<Item>('items');
+    this.items = this.itemsCollection.valueChanges();
+  }
+
+  addItem(item: Item)
+  {
+    this.itemsCollection.add(item);
+  }
 }

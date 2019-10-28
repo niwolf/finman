b5fb5bedef6b056332fd53a56b41fc83a938b1d1
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-10-28 16:34:28 +0100

    encapsulate functionality in the ButtonComponent

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component
+} from '@angular/core';
 
 @Component({
-  selector: 'fin-button',
+  selector:    'fin-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls:   ['./button.component.scss']
 })
-export class ButtonComponent {
-
+export class ButtonComponent implements AfterViewInit {
   toggled = false;
 
-  public toggle(): void
-  {
+  private readonly backdrop: HTMLDivElement = document.createElement('div');
+
+  public ngAfterViewInit(): void {
+    this.insertBackdropElement();
+  }
+
+  public toggle(): void {
     this.toggled = !this.toggled;
+    this.backdrop.style.visibility = this.toggled ? 'visible' : 'hidden';
+  }
+
+  private insertBackdropElement(): void {
+    this.backdrop.classList.value = 'mat-drawer-backdrop mat-drawer-shown';
+    this.backdrop.style.visibility = 'hidden';
+    this.backdrop.onclick = (): void => this.toggle();
+    document.body.insertAdjacentElement('beforeend', this.backdrop);
   }
 }

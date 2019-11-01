cfda8b035f718de3e3023024e00f55d33730bdc5
Niklas Wolf <niklas.wolf@plentymarkets.com>
2019-11-01 18:24:31 +0100

    add route for listings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {
   Routes
 } from '@angular/router';
 import { EnterDataComponent } from './enter-data/enter-data.component';
+import { ListingsComponent } from './listings/listings.component';
 
 
 const routes: Routes = [
@@ -15,6 +16,10 @@ const routes: Routes = [
   {
     path:      'enterData',
     component: EnterDataComponent
+  },
+  {
+    path:      'listings',
+    component: ListingsComponent
   }
 ];
 

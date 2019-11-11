e8a036689ba06bf74b48700a284237db141d65b6
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-11-11 12:37:26 +0100

    Create a sample dashboard

---
+++ src/app/dashboard/tiles/balance/balance.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { switchMap, map } from 'rxjs/operators';
+
+@Component({
+  selector:    'fin-balance',
+  templateUrl: './balance.component.html',
+  styleUrls:   ['./balance.component.scss']
+})
+export class BalanceComponent implements OnInit {
+
+  balance$: Observable<{cash: number, account: number}>;
+
+  constructor(private db: AngularFirestore, private auth: AngularFireAuth) { }
+
+  ngOnInit() {
+    this.balance$ = this.auth.user.pipe(switchMap(user =>
+    {
+      const uid: string = user.uid;
+      return this.db.doc<any>(`/users/${uid}`).get().pipe(map(snapshot => snapshot.get('initialBudget')));
+    }));
+  }
+
+}

a7c3f41c4bca8ac0f77aa9d1c534622fc6495bef
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-11-14 12:47:57 +0100

    handle all write events

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,6 +8,10 @@ import DocumentSnapshot = admin.firestore.DocumentSnapshot;
 import CollectionReference = admin.firestore.CollectionReference;
 import Timestamp = admin.firestore.Timestamp;
 
+import Change = functions.Change;
+import EventContext = functions.EventContext;
+
+
 
 interface Item {
   title: string;
@@ -18,12 +22,57 @@ interface Item {
 
 const db = admin.firestore();
 
-// Listens for new items created by a user and re-calculates his current budget
-exports.updateBudget = functions.firestore.document('/users/{userId}/items/{itemId}').onCreate(async (snapshot, context) =>
+async function initCurrentBudget(userDocSnap: DocumentSnapshot, userId:string, userDoc: DocumentReference)
 {
-  // Grab the current value of what was written to the Realtime Database.
-  const value = snapshot.get('value');
-  const origin = snapshot.get('origin');
+  const initialBudget: { cash: number, account: number } = userDocSnap.get('initialBudget');
+  const itemCollection: CollectionReference = db.collection(`/users/${userId}/items`);
+  const itemDocRefs: DocumentReference[] = await itemCollection.listDocuments();
+  const itemDocPromises: Promise<DocumentSnapshot>[] = itemDocRefs.map(doc => doc.get());
+  const itemDocSnaps: DocumentSnapshot[] = await Promise.all(itemDocPromises);
+  const items: Item[] = itemDocSnaps.map(snap => snap.data() as Item);
+
+  const reducer = (acc: number, curr: Item) => acc + curr.value;
+  const cashSum: number = items.filter(item => item.origin === 'cash').reduce(reducer, 0);
+  const accountSum: number = items.filter(item => item.origin === 'account').reduce(reducer, 0);
+
+  const newBudget = {
+    cash:    initialBudget.cash + cashSum,
+    account: initialBudget.account + accountSum
+  };
+
+  console.log('Budget created for user', userId);
+  return userDoc.update('currentBudget', newBudget);
+}
+
+function updateCurrentBudget(currentBudget: { cash: number; account: number }, change:Change<DocumentSnapshot>, userId:string, userDoc:DocumentReference)
+{
+  // Get an object with the current document value.
+  // If the document does not exist, it has been deleted.
+  const doc = change.after.exists ? change.after : change.before;
+
+  const origin = doc.get('origin');
+  let value:number = doc.get('value');
+  if(!change.after.exists) { value = change.before.get('value') * -1; } // document deleted
+  if(change.after.exists && change.before.exists) { value = change.after.get('value') - change.before.get('value') } // document updated
+
+  const newBudget: { cash: number, account: number } = {
+    cash:    currentBudget.cash,
+    account: currentBudget.account
+  };
+  if (origin === 'cash')
+  {
+    newBudget.cash += value;
+  } else if (origin === 'account')
+  {
+    newBudget.account += value;
+  }
+  console.log('Budget updated for user', userId);
+  return userDoc.update('currentBudget', newBudget);
+}
+
+async function updateBudget(change: Change<DocumentSnapshot>, context: EventContext)
+{
+  // Grab the current values.
   const userId = context.params.userId;
   const userDoc: DocumentReference = db.doc(`/users/${userId}`);
   const userDocSnap: DocumentSnapshot = await userDoc.get();
@@ -31,38 +80,13 @@ exports.updateBudget = functions.firestore.document('/users/{userId}/items/{item
 
   if (currentBudget)
   {
-    const newBudget: { cash: number, account: number } = {
-      cash:    currentBudget.cash,
-      account: currentBudget.account
-    };
-    if (origin === 'cash')
-    {
-      newBudget.cash += value;
-    } else if (origin === 'account')
-    {
-      newBudget.account += value;
-    }
-    console.log('Budget updated for user', userId);
-    return userDoc.update('currentBudget', newBudget);
+    return updateCurrentBudget(currentBudget, change, userId, userDoc);
   } else
   {
-    const initialBudget: { cash: number, account: number } = userDocSnap.get('initialBudget');
-    const itemCollection: CollectionReference = db.collection(`/users/${userId}/items`);
-    const itemDocRefs: DocumentReference[] = await itemCollection.listDocuments();
-    const itemDocPromises: Promise<DocumentSnapshot>[] = itemDocRefs.map(doc => doc.get());
-    const itemDocSnaps: DocumentSnapshot[] = await Promise.all(itemDocPromises);
-    const items: Item[] = itemDocSnaps.map(snap => snap.data() as Item);
-
-    const reducer = (acc: number, curr: Item) => acc + curr.value;
-    const cashSum: number = items.filter(item => item.origin === 'cash').reduce(reducer, 0);
-    const accountSum: number = items.filter(item => item.origin === 'account').reduce(reducer, 0);
-
-    const newBudget = {
-      cash:    initialBudget.cash + cashSum,
-      account: initialBudget.account + accountSum
-    };
-
-    console.log('Budget created for user', userId);
-    return userDoc.update('currentBudget', newBudget);
+    return initCurrentBudget(userDocSnap, userId, userDoc);
   }
-});
+}
+
+// Listens for new items created by a user and re-calculates his current budget
+exports.updateBudget = functions.firestore.document('/users/{userId}/items/{itemId}').onWrite(updateBudget);
+

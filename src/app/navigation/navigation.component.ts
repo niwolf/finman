c135f92aed5f1369c4d35f0e19d9477e09eeb4eb
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2019-11-15 09:43:56 +0100

    fix: show button only on dashboard and activity

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,6 +1,7 @@
 import {
   Component,
-  OnInit
+  OnInit,
+  ViewChild
 } from '@angular/core';
 import {
   BreakpointObserver,
@@ -14,12 +15,12 @@ import {
 } from 'rxjs/operators';
 import { AngularFireAuth } from '@angular/fire/auth';
 import * as md5 from 'md5';
-import { DashboardComponent } from '../dashboard/dashboard.component';
-import { ActivatedRoute } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { UserData } from '../models/user-data.interface';
 import { InitialBudgetDialogComponent } from '../dialogs/initial-budget-dialog/initial-budget-dialog.component';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { MatDialog } from '@angular/material';
+import { EnterDataComponent } from '../enter-data/enter-data.component';
 
 @Component({
   selector:    'fin-navigation',
@@ -41,12 +42,14 @@ export class NavigationComponent implements OnInit {
   private readonly defaultUserImg = `https://secure.gravatar.com/avatar/${md5(this.auth.auth.currentUser.email)}?d=mp`;
   userImg: string = this.auth.auth.currentUser.photoURL || this.defaultUserImg;
 
+  @ViewChild(RouterOutlet, {static: false})
+  private routerOutlet: RouterOutlet;
+
   constructor(
     private breakpointObserver: BreakpointObserver,
     private dialog: MatDialog,
     private auth: AngularFireAuth,
-    private db: AngularFirestore,
-    private route: ActivatedRoute
+    private db: AngularFirestore
   ) {}
 
   public ngOnInit(): void
@@ -76,9 +79,8 @@ export class NavigationComponent implements OnInit {
     })).subscribe();
   }
 
-  public get isDashboard(): boolean
-  {
-    return this.route.firstChild.component === DashboardComponent;
+  public get editorActivated(): boolean {
+    return this.routerOutlet ? this.routerOutlet.component instanceof EnterDataComponent : false;
   }
 
   public signOut(): void {

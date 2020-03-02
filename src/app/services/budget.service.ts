c8e9c46f63859f9db5dd5df8059bcc3bb0ed72ca
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2020-03-02 17:01:24 +0100

    a private method that returns the doc for the user

--- src/app/services/budget.service.ts
+++ src/app/services/budget.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { UserData } from '../models/user-data.interface';
 import { Budget } from '../models/budget.interface';
+import { AngularFirestoreDocument } from '@angular/fire/firestore/document/document';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +13,18 @@ export class BudgetService {
   constructor(private afs: AngularFirestore) {}
 
   public getInitialBudget(userId: string): Observable<Budget> {
-    return this.afs.doc<UserData>(`/users/${userId}`).get().pipe(map(snapshot => snapshot.get('initialBudget')));
+    return this.getUserDoc(userId).get().pipe(map(snapshot => snapshot.get('initialBudget')));
   }
 
   public setInitialBudget(userId: string, budget: Budget): Promise<void> {
-    return this.afs.doc<UserData>(`users/${userId}`).update({initialBudget: budget});
+    return this.getUserDoc(userId).update({initialBudget: budget});
   }
 
   public getCurrentBudget(userId: string): Observable<Budget> {
-    return this.afs.doc<UserData>(`/users/${userId}`).get().pipe(map(snapshot => snapshot.get('currentBudget')));
+    return this.getUserDoc(userId).get().pipe(map(snapshot => snapshot.get('currentBudget')));
+  }
+
+  private getUserDoc(userId: string): AngularFirestoreDocument<UserData> {
+    return this.afs.doc<UserData>(`/users/${userId}`);
   }
 }

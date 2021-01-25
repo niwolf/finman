f236f7e7325a658a14fc8a395e5bb48fe4a06e9b
Niklas Wolf <niklas.wolf@plentymarkets.com>
2021-01-25 17:19:00 +0100

    wip

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'fin-profile',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void
+  {
+  }
+
+  public get user(): any
+  {
+    return this.authService.currentUser.email;
   }
 
 }

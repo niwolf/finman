19bf3b3a8f2659cc857f5cd7b075c9b15dfc7c10
Niklas Wolf <niklas.wolf@plentymarkets.com>
2021-02-07 18:21:41 +0100

    wip

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,7 +2,9 @@ import {
   Component,
   OnInit
 } from '@angular/core';
-import { AuthService } from '../services/auth.service';
+import * as md5 from 'md5';
+import firebase from 'firebase';
+import { AuthService } from '../core/services/auth.service';
 
 @Component({
   selector: 'fin-profile',
@@ -11,7 +13,13 @@ import { AuthService } from '../services/auth.service';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  //@Input() set user(user: User) {
+  //  this.userImg = user.photoURL || `https://secure.gravatar.com/avatar/${md5(user.email)}?d=mp`;
+  //}
+
+  userImg: string;
+
+  constructor(public authService: AuthService) { }
 
   public hasProfilePic = false;
 
@@ -19,8 +27,12 @@ export class ProfileComponent implements OnInit {
   {
   }
 
-  public get user(): string
+  public get userMail(): string
   {
     return this.authService.currentUser.email;
+  }
+
+  public get userPic(): any {
+    return this.userImg = this.authService.currentUser.photoURL || `https://secure.gravatar.com/avatar/${md5(this.authService.currentUser.email)}?d=mp`;
   }
 }

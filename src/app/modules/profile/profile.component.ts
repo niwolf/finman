230e1d1d082a2dd94a41f591a2777cb0a13b9353
Pascal Weyrich <pascal.weyrich@plentymarkets.com>
2021-02-15 09:58:55 +0100

    Update profile.component.ts

--- src/app/modules/profile/profile.component.ts
+++ src/app/modules/profile/profile.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'fin-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProfileComponent {
   user$ = this.authService.currentUser$;
